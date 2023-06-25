01aef7e36dd29f77f566c51ba580832b166ff21e
Michael Wolf <susonicth@gmail.com>
2023-06-25 10:47:02 +0200

    added --numbers option; moved std* to Files struct

--- src/main.zig
+++ src/main.zig
@@ -12,60 +12,97 @@ fn print_version(file: std.fs.File) !void {
     try file.writeAll(version ++ license);
 }
 
+const Options = struct {
+    var outputNumbers: bool = false;
+};
+
+var line_number: u32 = 0;
+
+const File = struct {
+    var stdout: std.fs.File = undefined;
+    var stdin: std.fs.File = undefined;
+    var stderr: std.fs.File = undefined;
+};
+
 pub fn main() !void {
     var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{}){};
     const gpa = general_purpose_allocator.allocator();
     const args = try std.process.argsAlloc(gpa);
     defer std.process.argsFree(gpa, args);
 
-    const stdout = std.io.getStdOut();
-    const stdin = std.io.getStdIn();
+    File.stdout = std.io.getStdOut();
+    File.stdin = std.io.getStdIn();
+    File.stderr = std.io.getStdErr();
 
     if (args.len == 1) {
-        try copyFile(stdin);
+        try proccessFile(File.stdin);
     } else {
         for (args[1..]) |arg| {
             if (std.mem.eql(u8, arg, "--help")) {
-                try print_usage(stdout);
+                try print_usage(File.stdout);
                 std.os.exit(0);
             } else if (std.mem.eql(u8, arg, "--version")) {
-                try print_version(stdout);
+                try print_version(File.stdout);
                 std.os.exit(0);
+            } else if (std.mem.eql(u8, arg, "--number")) {
+                Options.outputNumbers = true;
+            } else if (std.mem.eql(u8, arg[0..1], "-") and !std.mem.eql(u8, arg[1..1], "-")) {
+                for (arg[1..]) |opt| {
+                    if (opt == 'n') {
+                        Options.outputNumbers = true;
+                    }
+                }
             } else if (std.mem.eql(u8, arg[0..2], "--") or (std.mem.eql(u8, arg[0..1], "-") and arg.len != 1)) {
                 try argument_error(gpa, arg);
                 std.os.exit(1);
             } else if (std.mem.eql(u8, arg, "-")) {
-                try copyFile(stdin);
+                try proccessFile(File.stdin);
             } else {
-                try copyFileByName(arg);
+                try processFileByName(arg);
             }
         }
     }
 }
 
 fn argument_error(allocator: std.mem.Allocator, arg: []u8) !void {
-    const stderr = std.io.getStdErr();
-    try print_usage(stderr);
+    try print_usage(File.stderr);
     const message = std.fmt.allocPrint(allocator, "\nError: argument '{s}' is unknown\n", .{arg}) catch unreachable;
     defer allocator.free(message);
-    try stderr.writeAll(message);
+    try File.stderr.writeAll(message);
 }
 
-fn copyFileByName(name: []const u8) !void {
+fn processFileByName(name: []const u8) !void {
     var path_buffer: [std.fs.MAX_PATH_BYTES]u8 = undefined;
     const path = try std.fs.realpath(name, &path_buffer);
     const file = try std.fs.openFileAbsolute(path, .{});
     defer file.close();
 
-    try copyFile(file);
+    try proccessFile(file);
+}
+
+fn proccessFile(in: std.fs.File) !void {
+    if (Options.outputNumbers) {
+        return processLines(in);
+    }
+    return copyFile(in);
 }
 
 fn copyFile(in: std.fs.File) !void {
-    const stdout = std.io.getStdOut();
     var buffer: [1024]u8 = undefined;
     var read = try in.readAll(&buffer);
     while (read > 0) {
-        try stdout.writeAll(buffer[0..read]);
+        try File.stdout.writeAll(buffer[0..read]);
         read = try in.readAll(&buffer);
+    }
+}
+
+fn processLines(in: std.fs.File) !void {
+    var buffered_Reader = std.io.bufferedReader(in.reader());
+    var reader = buffered_Reader.reader();
+    var buffer: [1024]u8 = undefined;
+
+    while (try reader.readUntilDelimiterOrEof(&buffer, '\n')) |line| {
+        line_number += 1;
+        try std.fmt.format(File.stdout.writer(), "{d: >6}\t{s}\n", .{ line_number, line });
     }
 }
